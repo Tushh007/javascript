34029231e3db20fa890c31636917790c2b2d5623
Tushar Langer <tushar.langer@joulon.com>
2020-10-10 21:02:49 +0530

    worked with arrays

---
+++ arrays/notes.js
@@ -0,0 +1,18 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+// console.log(notes)
+// console.log(notes.pop())
+// notes.push('My new note.')
+
+// console.log(notes.shift())
+
+// console.log(notes.unshift('haha note'))
+
+
+// console.log(notes)
+// console.log(notes.length)
+// console.log(notes[notes.length - 1])
+
+
+notes.splice(1, 0, 'this is the new item.')
+console.log(notes)
---
+++ arrays/todo.js
@@ -0,0 +1,19 @@
+// create a list of 5 items of to-do
+let todos = ['Wake up at 7', 'Study for the test', 'Excercise', 'Bake the Cake', 'Clean Home']
+
+// console.log(`You have ${todos.length} to-do items!`)
+// console.log(`Todo: ${todos[0]}`)
+// console.log(`Todo: ${todos[todos.length - 2]}`)
+
+console.log(todos)
+
+// delete the 3rd item
+todos.splice(2, 1)
+
+// add a new item to the end
+todos.push('added new item at the end')
+
+// remove the first item 
+todos.shift()
+
+console.log(todos)
