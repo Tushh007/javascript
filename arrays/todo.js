2f2fc996a7b197c8ab54977fd00a40da5661fec0
Tushar Langer <tushar.langer@joulon.com>
2020-10-12 13:52:51 +0530

    sort

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,113 +1,147 @@
-// create a list of 5 items of to-do
-let todos = [
-  "Wake up at 7",
-  "Study for the test",
-  "Excercise",
-  "Bake the Cake",
-  "Clean Home",
-];
+// // create a list of 5 items of to-do
+// let todos = [
+//   "Wake up at 7",
+//   "Study for the test",
+//   "Excercise",
+//   "Bake the Cake",
+//   "Clean Home",
+// ];
 
-console.log(`You have ${todos.length} to-do items!`);
-console.log(`Todo: ${todos[0]}`);
-console.log(`Todo: ${todos[todos.length - 2]}`);
+// console.log(`You have ${todos.length} to-do items!`);
+// console.log(`Todo: ${todos[0]}`);
+// console.log(`Todo: ${todos[todos.length - 2]}`);
 
-console.log(todos);
+// console.log(todos);
 
-// delete the 3rd item
-todos.splice(2, 1);
+// // delete the 3rd item
+// todos.splice(2, 1);
 
-// add a new item to the end
-todos.push("added new item at the end");
+// // add a new item to the end
+// todos.push("added new item at the end");
 
-// remove the first item
-todos.shift();
+// // remove the first item
+// todos.shift();
 
-console.log(todos);
+// console.log(todos);
 
-todos.forEach(function (item, index) {
-  console.log(`${index + 1}. ${item}`);
-});
+// todos.forEach(function (item, index) {
+//   console.log(`${index + 1}. ${item}`);
+// });
 
-console.log("");
+// console.log("");
 
-for (let index = 0; index < todos.length; index++) {
-  console.log(`${index + 1}. ${todos[index]}`);
-}
+// for (let index = 0; index < todos.length; index++) {
+//   console.log(`${index + 1}. ${todos[index]}`);
+// }
 
-console.log("");
+// console.log("");
 
-console.log(todos.indexOf("Excercise"));
+// console.log(todos.indexOf("Excercise"));
 
 const todos = [
   {
-    text: "Todo 1",
+    text: "Todo 3",
     completed: true,
+  },
+  {
+    text: "Todo 1",
+    completed: false,
   },
   {
     text: "Todo 2",
     completed: false,
   },
   {
-    text: "Todo 3",
+    text: "Todo 4",
     completed: false,
   },
 ];
 
-const searchToDo = function (todos, query) {
-  return todos.filter(function (todo) {
-    const isTextMatch = todo.text.toLowerCase().includes(query.toLowerCase());
-    return isTextMatch;
-  });
-};
+const sortTodos = function (todos) {
+    todos.sort(function(a, b) {
+        if (a.text.toLowerCase() < b.text.toLowerCase()) {
+            return -1
+        } else if (b.text.toLowerCase() < a.text.toLowerCase()) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
 
-console.log(searchToDo(todos, "do 1"));
+const sortTodosByStatus = function (todos) {
+    todos.sort(function (a, b) {
+        if(!a.completed && b.completed) {
+            return -1
+        } else if (!b.completed && a.completed) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
 
-const getThingsToDo = function (todos) {
-  return todos.filter(function (todo) {
-    return !todo.completed;
-  });
-};
+sortTodos(todos)
+console.log(todos)
 
-console.log(getThingsToDo(todos));
+sortTodosByStatus(todos)
+console.log(todos)
 
-const deleteTodo = function (todos, todoText) {
-  const index = todos.findIndex(function (todo) {
-    return todo.text.toLowerCase() === todoText.toLowerCase();
-  });
+// const searchToDo = function (todos, query) {
+//   return todos.filter(function (todo) {
+//     const isTextMatch = todo.text.toLowerCase().includes(query.toLowerCase());
+//     return isTextMatch;
+//   });
+// };
 
-  // delete the note
-  if (index > -1) {
-    todos.splice(index, 1);
-  }
-};
+// console.log(searchToDo(todos, "do 1"));
 
-deleteTodo(todos, "Todo 5");
-console.log(todos);
+// const getThingsToDo = function (todos) {
+//   return todos.filter(function (todo) {
+//     return !todo.completed;
+//   });
+// };
 
-console.log(todos);
+// console.log(getThingsToDo(todos));
 
-const index = todos.findIndex(function (todo, index) {
-  console.log(index, todo);
-  return todo.title === "Todo 3";
-});
+// const deleteTodo = function (todos, todoText) {
+//   const index = todos.findIndex(function (todo) {
+//     return todo.text.toLowerCase() === todoText.toLowerCase();
+//   });
 
-console.log(index);
+//   // delete the note
+//   if (index > -1) {
+//     todos.splice(index, 1);
+//   }
+// };
 
-const findTodo = function (todos, todoTitle) {
-  const index = todos.findIndex(function (todo, index) {
-    return todo.title.toLowerCase() === todoTitle.toLowerCase();
-  });
+// deleteTodo(todos, "Todo 5");
+// console.log(todos);
 
-  return todos[index];
-};
+// console.log(todos);
 
-const findTodo = function (todos, todoTitle) {
-  const todo = todos.find(function (todo, index) {
-    return todo.title.toLowerCase() === todoTitle.toLowerCase();
-  });
+// const index = todos.findIndex(function (todo, index) {
+//   console.log(index, todo);
+//   return todo.title === "Todo 3";
+// });
 
-  return todo;
-};
+// console.log(index);
 
-const todo = findTodo(todos, "TODO 5");
-console.log(todo);
+// const findTodo = function (todos, todoTitle) {
+//   const index = todos.findIndex(function (todo, index) {
+//     return todo.title.toLowerCase() === todoTitle.toLowerCase();
+//   });
+
+//   return todos[index];
+// };
+
+// const findTodo = function (todos, todoTitle) {
+//   const todo = todos.find(function (todo, index) {
+//     return todo.title.toLowerCase() === todoTitle.toLowerCase();
+//   });
+
+//   return todo;
+// };
+
+// const todo = findTodo(todos, "TODO 5");
+// console.log(todo);
