913b0a5efbd5efc589aae598ed8c86f491539306
Tushar Langer <tushar.langer@joulon.com>
2020-10-12 12:56:58 +0530

    todos

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,9 +1,9 @@
 // create a list of 5 items of to-do
 let todos = ['Wake up at 7', 'Study for the test', 'Excercise', 'Bake the Cake', 'Clean Home']
 
-// console.log(`You have ${todos.length} to-do items!`)
-// console.log(`Todo: ${todos[0]}`)
-// console.log(`Todo: ${todos[todos.length - 2]}`)
+console.log(`You have ${todos.length} to-do items!`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
 
 console.log(todos)
 
@@ -13,7 +13,7 @@ todos.splice(2, 1)
 // add a new item to the end
 todos.push('added new item at the end')
 
-// remove the first item 
+// remove the first item
 todos.shift()
 
 console.log(todos)
@@ -30,4 +30,61 @@ for (let index = 0; index < todos.length; index++) {
 
 console.log('')
 
-console.log(todos.indexOf('Bake the Cake'))
+console.log(todos.indexOf('Excercise'))
+
+const todos = [
+  {
+    text: "Todo 1",
+    completed: true
+  },
+  {
+    text: "Todo 2",
+    completed: false
+  },
+  {
+    text: "Todo 3",
+    completed: true
+  },
+];
+
+const deleteTodo = function (todos, todoText) {
+  const index = todos.findIndex(function (todo) {
+    return todo.text.toLowerCase() === todoText.toLowerCase();
+  });
+
+  // delete the note
+  if (index > -1) {
+    todos.splice(index, 1)
+  }
+};
+
+deleteTodo(todos, "Todo 5");
+console.log(todos);
+
+console.log(todos)
+
+const index = todos.findIndex(function (todo, index) {
+    console.log(index, todo)
+    return todo.title === 'Todo 3'
+})
+
+console.log(index)
+
+const findTodo = function (todos, todoTitle) {
+    const index = todos.findIndex(function (todo, index) {
+        return todo.title.toLowerCase() === todoTitle.toLowerCase()
+    })
+
+    return todos[index]
+}
+
+const findTodo = function (todos, todoTitle) {
+    const todo = todos.find(function (todo, index) {
+        return todo.title.toLowerCase() === todoTitle.toLowerCase()
+    })
+
+    return todo
+}
+
+const todo = findTodo(todos, 'TODO 5')
+console.log(todo)
